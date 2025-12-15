09e34ac4b182d729e3becd8fd7c3ddf7051b725d
Ga Min Cha <prettygamin@naver.com>
2025-12-15 15:17:22 +0900

    initial code

---
+++ yolov5/changedetection.py
@@ -0,0 +1,86 @@
+import os
+import cv2
+import pathlib
+import requests
+from datetime import datetime
+
+class ChangeDetection:
+    result_prev = []
+    HOST = 'https://gmcha.pythonanywhere.com' #'http://10.0.2.2:8000'
+    username = 'admin'
+    password = 'finalterm'
+    token = ''
+    title = ''
+    text = ''
+
+    def __init__(self, names):
+        self.result_prev = [0 for i in range(len(names))]
+
+        res = requests.post(self.HOST + '/api-token-auth/', {
+            'username': self.username,
+            'password': self.password,
+        })
+        res.raise_for_status()
+        self.token = res.json()['token']
+        print(self.token)
+
+    def add(self, names, detected_current, save_dir, image):
+        self.title = ''
+        self.text = ''
+        change_flag = 0
+        newly_detected_names = [] #added
+
+        i = 0
+        while i < len(self.result_prev):
+            if self.result_prev[i]==0 and detected_current[i]==1 :
+                change_flag = 1
+                # self.title = names[i]
+                # self.text += names[i] + ", "
+                newly_detected_names.append(names[i]) # added
+            i += 1
+
+        self.result_prev = detected_current[:]
+
+        if change_flag==0:
+            return
+
+        # -- added --
+        if 'book' in newly_detected_names:
+            self.title = "book!"
+            self.text = "book! 책이 등장했습니다. 빨리 가지러 가세요."
+        else:
+            self.title = newly_detected_names[0] 
+            self.text = ", ".join(newly_detected_names) + " (이)가 새로 검출되었습니다."
+        # --- 
+
+        # if change_flag==1:
+        #     self.send(save_dir, image)
+        self.send(save_dir, image) # added
+
+    def send(self, save_dir, image):
+        now = datetime.now()
+        now.isoformat()
+        
+        today = datetime.now()
+        save_path = os.getcwd() / save_dir / 'detected' / str(today.year) / str(today.month) / str(today.day) 
+        pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
+
+        full_path = save_path / '{0}-{1}-{2}-{3}.jpg'.format(today.hour,today.minute,today.second,today.microsecond)
+
+        dst = cv2.resize(image, dsize=(320, 240), interpolation=cv2.INTER_AREA)
+        cv2.imwrite(full_path, dst)
+
+        # 인증이 필요한 요청에 아래의 headers를 붙임
+        headers = {'Authorization' : 'JWT ' + self.token, 'Accept' : 'application/json'}
+
+        # Post Create
+        data = {
+        'author' : 1,
+        'title' : self.title, 
+        'text' : self.text, 
+        'created_date' : now, 
+        'published_date' : now
+        }
+        file = {'image' : open(full_path, 'rb')}
+        res = requests.post(self.HOST + '/api_root/Post/', data=data, files=file, headers=headers)
+        print(res)
